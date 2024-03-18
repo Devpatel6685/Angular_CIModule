2ab9701033579701fe3c8f308d8ba2fe69f5e72e
SERVER7\PCE63 <devv.patel@internal.mail>
2024-03-18 10:03:27 +0530

    Constants

---
+++ Common/Constants.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIPLATFORM.Common
+{
+    public class Constants
+    {
+        public static string SUCCESS = "Success!";
+        public static string USER_NOT_FOUND = "User not found!!";
+        public static string RESET_PASSWORD_EMAIL_SUCCESS = "Reset Password email sent successfully!";
+        public static string RESET_PASSWORD_SUCCESS = "Password reset successfully!";
+        public static string ERROR = "Oops, Something went wrong. Please try again.";
+        public static string NO_DATA = "No Data Found!";
+    }
+}
